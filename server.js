5ad7a5d63173b69d68bae7ffe2e83bb4d62874c4
rangerkoala <sebastiancamilopf@gmail.com>
2026-02-02 17:22:47 -0500

    render failed. fixed parameters.

--- server.js
+++ server.js
@@ -33,8 +33,8 @@ app.use("/inv", inventoryRoute)
  * Local Server Information
  * Values from .env (environment) file
  *************************/
-const port = process.env.PORT
-const host = process.env.HOST
+const port = process.env.PORT || 5500
+const host = process.env.HOST || "localhost"
 
 /* ***********************
  * Log statement to confirm server operation
