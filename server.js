fa8beb02d28028dc1b14818718a0fb6f3908f2ea
rangerkoala <sebastiancamilopf@gmail.com>
2026-02-05 15:39:59 -0500

    database url

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.get("/", baseController.buildHome)
 // Inventory routes
 app.use("/inv", inventoryRoute)
 
-/* ***********************
+/* **********************
  * Local Server Information
  * Values from .env (environment) file
  *************************/
